16ebf29407510d96aeaa4c2ab4d23992737197fb
lubomirjurukov <lasi123456@gmail.com>
2014-09-12 23:43:16 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main{
+
+	public static void main(String[] args) {
+		JFrame mainFrame = new JFrame("");
+		mainFrame.setSize(800, 600);
+    	mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+    	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    	Panel panel = new Panel();
+    	mainFrame.add(panel);
+    	mainFrame.setVisible(true);
+	}
+}

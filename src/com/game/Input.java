1c2c36d560b31b88dd98163ac261f56b62415283
sashky90 <sashky@ymail.com>
2014-09-14 18:42:04 +0300

    Updated Input

--- src/com/game/Input.java
+++ src/com/game/Input.java
@@ -3,14 +3,53 @@ package com.game;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class Input implements KeyListener{
-    public void keyTyped(KeyEvent e) {}
 
-    public void keyPressed(KeyEvent e) {
-    	
-    }
+public class Input implements KeyListener {
+	public boolean up = false;
+	public boolean down = false;
+	public boolean left = false;
+	public boolean right = false;
 
-    public void keyReleased(KeyEvent e) {
-    	
-    }
+	public void keyTyped(KeyEvent e) {
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+
+			up = true;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+
+			down = true;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+
+			right = true;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+
+			left = true;
+
+		}
+
+		if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+			System.exit(0);
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+			up = false;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+			down = false;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			right = false;
+
+		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+			left = false;
+
+		}
+	}
 }

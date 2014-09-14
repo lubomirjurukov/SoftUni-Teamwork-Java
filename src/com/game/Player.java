faee4a1f2dfd2c845d9330a32f19f68bff3ba2e7
lubomirjurukov <lasi123456@gmail.com>
2014-09-15 00:34:09 +0300

    Updated Player and png

--- src/com/game/Player.java
+++ src/com/game/Player.java
@@ -6,32 +6,36 @@ import java.awt.Toolkit;
 
 import javax.swing.JPanel;
 
-public class Player extends JPanel{
+public class Player extends JPanel {
 	private Image image = Toolkit.getDefaultToolkit().createImage("res/penguin.png");
 	int x = 0;
 	int y = 0;
-	
-	public int getX(){
+	int width = 50;
+	int height = 50;
+
+	public int getX() {
 		return this.x;
 	}
-	public void setX(int x){
+
+	public void setX(int x) {
 		this.x = x;
 	}
-	
-	public int getY(){
+
+	public int getY() {
 		return this.y;
 	}
-	
-	public void setY(int y){
+
+	public void setY(int y) {
 		this.y = y;
 	}
-	
-	public void paintComponent(Graphics g){
+
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		g.drawImage(image,x,y,null);
+		g.drawImage(image, x, y, width, height, null);
 	}
-	
-	public boolean detectCollision(Enemy enemy){
-			return (enemy.getX()-x)*(enemy.getX()-x) + (enemy.getY()-y)*(enemy.getY()-y) <= (50 * 50);
+
+	public boolean detectCollision(Enemy enemy) {
+		return (enemy.getX() - (x+width/2)) * (enemy.getX() - (x+width/2)) + (enemy.getY() - (y+height/2))
+				* (enemy.getY() - (y+height/2)) <= (width/2 * width/2);
 	}
 }

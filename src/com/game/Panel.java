2665252838007d1111284c4363606942af9d290f
lubomirjurukov <lasi123456@gmail.com>
2014-09-14 19:37:52 +0300

    Updated Panel

--- src/com/game/Panel.java
+++ src/com/game/Panel.java
@@ -1,13 +1,19 @@
 package com.game;
 //import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
 public class Panel extends JPanel implements ActionListener{
 	private Input input = new Input();
+	private Image image = Toolkit.getDefaultToolkit().createImage("res/penguin.png");
 	private int timer = 0;
     //construct a Panel
     public Panel(){
@@ -25,11 +31,18 @@ public class Panel extends JPanel implements ActionListener{
     }
 
     public void step(){
-    	if(timer==10&&!(Main.enemys.size()>40)){
+    	if(input.up&&Main.player.y>=0){
+    		Main.player.y-=3;
+    	}
+    	if(input.down&&Main.player.y<=Main.mainFrame.getHeight()-50){
+    		Main.player.y+=3;
+    	}
+    	if(timer==10&&!(Main.enemys.size()>20)){
     		Main.enemys.add(new Enemy());
     		timer=0;
     	}
         for(int i = 0;i<Main.enemys.size();i++){
+        	Main.player.detectCollision(Main.enemys.get(i));
         	Main.enemys.get(i).update();;
         } 
     	timer++;
@@ -38,6 +51,7 @@ public class Panel extends JPanel implements ActionListener{
     
     public void paintComponent(Graphics g){
         super.paintComponent(g);
+        //g.drawImage(image, 0, 0, null);
         Main.player.paintComponent(g);
         for(int i = 0;i<Main.enemys.size();i++){
         	Main.enemys.get(i).paintComponent(g);

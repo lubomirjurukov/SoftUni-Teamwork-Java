c3662e0994cebb6a29c442a9330871a2757eef38
archlicher <christophergbbg@yahoo.com>
2014-09-14 15:47:02 +0300

    Player setters and getters

--- src/com/game/Player.java
+++ src/com/game/Player.java
@@ -4,9 +4,43 @@ import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class Player extends JPanel{
-
+	
+	int x = 0;
+	int y = 0;
+	boolean alive = true;
+	
+	public int getX(){
+		return this.x;
+	}
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public void setLive(boolean death) {
+		this.alive = death;
+	}
+	
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
-		g.fillOval(10, 50, 20, 20);
+		g.fillOval(x, y, 20, 20);
 	}
+	
+	public boolean detectCollision(Enemy enemy){
+		int a = enemy.getX();
+		int b = enemy.getY();
+		if (this.x == a && this.y == b) {
+			return true;
+		} 
+		else {
+			return false;}
+	}
+	
 }

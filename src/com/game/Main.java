ef9a2593e86a72c880ad9b5b1c2020b8a17116ff
lubomirjurukov <lasi123456@gmail.com>
2014-09-14 15:22:47 +0300

    Updated Main

--- src/com/game/Main.java
+++ src/com/game/Main.java
@@ -3,8 +3,9 @@ import javax.swing.JFrame;
 
 public class Main{
 	public static Player player = new Player();
+	public static JFrame mainFrame = new JFrame("");
 	public static void main(String[] args) {
-		JFrame mainFrame = new JFrame("");
+		
 		mainFrame.setSize(800, 600);
     	mainFrame.setExtendedState(JFrame.MAXIMIZED_BOTH);
     	mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
